830136e31f18bda001d8592fb5ab776e8f718810
Vincent MONTECOT <vmonteco@e1r10p11.42.fr>
2015-10-25 15:00:22 +0100

    itoa test started

--- src/test_itoa.c
+++ src/test_itoa.c
@@ -1,1 +1,51 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   test_itoa.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: vmonteco <vmonteco@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2015/10/25 13:06:47 by vmonteco          #+#    #+#             */
+/*   Updated: 2015/10/25 13:13:34 by vmonteco         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
 
+#include <stdlib.h>
+#include "libft.h"
+#include "test.h"
+
+/*
+** This function should return the int value from a char * conversion.
+*/
+
+static int		log(int arg1, char *res, char *res)
+{
+	printf("ERROR: file %s.\nArgs : %d (int).\nExpected : %d (char *).\nGot : %\
+d (int).",
+		   __file__, arg1, exp, res);
+	return (0);
+}
+
+static int		test_case(int arg1, char *exp)
+{
+	char	*res;
+	char	*exp;
+
+	res = ft_itoa(arg1);
+	exp = itoa(arg1);
+	if (res == exp)
+		return (1);
+	log(arg1, exp, res);
+	return (0);
+}
+
+int			test_itoa(void)
+{
+	if (test_case(0, "0"))
+	{
+		SUCCESS_LOG
+		return (1);
+	}
+	ERROR_LOG
+	return (0);
+}
