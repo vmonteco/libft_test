7c6f0652fff923a01542b8a4f58aaf0cb9edd274
vincent montecot <vmonteco@student.42.fr>
2015-11-17 08:07:57 +0100

    debug added totest_atoi.c

--- mytests/src/test_atoi.c
+++ mytests/src/test_atoi.c
@@ -6,7 +6,7 @@
 /*   By: vmonteco <vmonteco@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2015/10/25 11:00:22 by vmonteco          #+#    #+#             */
-/*   Updated: 2015/11/17 03:58:40 by vmonteco         ###   ########.fr       */
+/*   Updated: 2015/11/17 06:51:38 by vmonteco         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -34,15 +34,17 @@ static int		test_case(char *arg1)
 
 	res = ft_atoi(arg1);
 	exp = atoi(arg1);
+	printf("test1");
 	if (res == exp)
 		return (1);
+	printf("test2");
 	log_error(arg1, exp, res);
 	return (0);
 }
 
 int			test_atoi(void)
 {
-	if (test_case("0"))
+	if (test_case("0") == 1)
 	{
 		SUCCESS_LOG
 		return (1);
