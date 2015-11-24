451aec65e1028161fab28bcebc2077f08b464573
Vincent MONTECOT <vmonteco@e2r10p9.42.fr>
2015-11-24 18:06:45 +0100

    test_lstnew.c added, has to be done

---
+++ src/test_lstnew.c
@@ -0,0 +1,85 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   test_lstadd.c                                     :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: vmonteco <vmonteco@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2015/11/24 05:28:45 by vmonteco          #+#    #+#             */
+/*   Updated: 2015/11/24 06:30:50 by vmonteco         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <stdlib.h>
+#include <stdio.h>
+#include <unistd.h>
+#include <ctype.h>
+#include "libft.h"
+#include "test.h"
+
+/*
+** This function checks if the given char is a number or a letter.
+*/
+
+static int		log_error(int arg1, int exp, int res)
+{
+	ERROR_LOG
+	printf("file %s.\nArgs : %d (int).\nExpected : %d (int).\nGot :\
+ %d (int).\n",
+		   __FILE__, arg1, exp, res);
+	return (0);
+}
+
+static int		test_case(int arg1)
+{
+	int		res;
+	int		exp;
+	pid_t	pid;
+	int		status;
+	
+	pid = fork();
+	status = 0;
+	if (pid < 0)
+	{
+		FORK_ERROR
+	}
+	else if (pid > 0)
+	{
+		wait(&status);
+		if (WIFSIGNALED(status) && WTERMSIG(status) == SIGSEGV)
+		{
+			printf(" - Case : %d ", arg1);
+			SEGFAULT_ERROR
+		}
+	}
+	else if (pid == 0)
+	{
+		res = ft_lstadd(arg1);
+		exp = lstadd(arg1);
+		if (res == exp)
+			exit(EXIT_SUCCESS);
+		log_error(arg1, exp, res);
+		exit(EXIT_FAILURE);
+	}
+	return (!status);
+}
+
+int			test_lstadd(void)
+{
+	t_list	*root;
+	t_list	elem1;
+	t_list	elem2;
+	void	*cases[][2] {{NULL, NULL}, {}}
+	int	i;
+
+	i = -300;
+	NAME_LOG("ft_lstadd()")
+	while (i < 300)
+	{
+		if (test_case(i) != 1)
+			return (0);
+		i++;
+	}
+	SUCCESS_LOG
+	return (1);
+}
