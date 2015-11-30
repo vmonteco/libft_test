38abb7122bd1dcd3a25a17a7557464aad837daa0
vincent montecot <vmonteco@student.42.fr>
2015-11-30 05:34:38 +0100

    test_lstiter seems OK, all lst functions seem OK.

--- includes/test.h
+++ includes/test.h
@@ -6,7 +6,7 @@
 /*   By: vmonteco <vmonteco@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2015/10/24 10:57:03 by vmonteco          #+#    #+#             */
-/*   Updated: 2015/11/30 01:49:52 by vmonteco         ###   ########.fr       */
+/*   Updated: 2015/11/30 05:07:41 by vmonteco         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -110,5 +110,6 @@ int					test_lstnew(void);
 int					test_lstdelone(void);
 int					test_lstdel(void);
 int					test_lstmap(void);
+int					test_lstiter(void);
 
 #endif
--- main.c
+++ main.c
@@ -6,7 +6,7 @@
 /*   By: vmonteco <vmonteco@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2015/11/17 04:22:52 by vmonteco          #+#    #+#             */
-/*   Updated: 2015/11/30 01:49:32 by vmonteco         ###   ########.fr       */
+/*   Updated: 2015/11/30 05:11:27 by vmonteco         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -28,58 +28,96 @@
 #define TEST_LSTDELONE
 #define TEST_LSTDEL
 #define TEST_LSTMAP
+#define DEP
 
 int		main(void)
 {
 #ifdef TEST_ATOI	
+# ifdef DEP
+# endif
 	test_atoi();
 #endif
 #ifdef TEST_ABSOLUTE
+# ifdef DEP
+# endif
 	test_absolute();
 #endif
 #ifdef TEST_BZERO
+# ifdef DEP
+# endif
 	test_bzero();
 #endif
 #ifdef TEST_ISALNUM
+# ifdef DEP
+# endif
 	test_isalnum();
 #endif
 #ifdef TEST_ISALPHA
+# ifdef DEP
+# endif
 	test_isalpha();
 #endif
 #ifdef TEST_ISASCII
+# ifdef DEP
+# endif
 	test_isascii();
 #endif
 #ifdef TEST_ISDIGIT
+# ifdef DEP
+# endif
 	test_isdigit();
 #endif
 #ifdef TEST_ISLOWER
+# ifdef DEP
+# endif
 	test_islower();
 #endif
 #ifdef TEST_ISPRINT
+# ifdef DEP
+# endif
 	test_isprint();
 #endif
 #ifdef TEST_ISUPPER
+# ifdef DEP
+# endif
 	test_isupper();
 #endif
 #ifdef TEST_ITOA
+# ifdef DEP
+# endif
 	test_itoa();
 #endif
 #ifdef TEST_LSTNEW
+# ifdef DEP
+# endif
 	test_lstnew();
 #endif
 #ifdef TEST_LSTADD
+# ifdef DEP
 	printf("/!\\ ft_lstadd() requires ft_lstnew(). /!\\\n");
+# endif
 	test_lstadd();
 #endif
 #ifdef TEST_LSTDELONE
+# ifdef DEP
+# endif
 	test_lstdelone();
 #endif
 #ifdef TEST_LSTDEL
+# ifdef DEP
 	printf("/!\\ ft_lstdel() requires ft_lstdelone.  /!\\\n");
+# endif
 	test_lstdel();
 #endif
 #ifdef TEST_LSTMAP
+# ifdef DEP
 	printf("/!\\ ft_lstmap() requires ft_memalloc(). /!\\\n");
+# endif
+	test_lstmap();
+#endif
+#ifdef TEST_LSTITER
+# ifdef DEP
+# endif
 	test_lstmap();
 #endif
 	return (0);
